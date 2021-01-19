5f57e21571f5969799d91cb9b981f61dfa1355e3
Alex <alexiscaronparis@outlook.fr>
2021-01-19 19:18:15 +0100

    fix psutil issue

--- Get_PC_Infos.py
+++ Get_PC_Infos.py
@@ -1,12 +1,14 @@
-import platform, os, subprocess, re
-
+import platform, os, subprocess, re, sys
+import psutil
 
 def Get_CPU_NAME():  ## Return CPU Name depending on which platform the program is Used
      if platform.system() == "Darwin":
         os.environ['PATH'] = os.environ['PATH'] + os.pathsep + '/usr/sbin'
         cpu_name = os.popen('sysctl -n machdep.cpu.brand_string').read()
-        print('CPU IS : {}'.format(cpu_name))
-    
+        cpu_load = [x / os.cpu_count() * 100 for x in os.getloadavg()][-1]
+        cpu_load = int(cpu_load)
+        print('cpu name : {} \ncpu load : {}'.format(cpu_name, cpu_load))
+        print(psutil.cpu_percent())
 
 
 Get_CPU_NAME()
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Get_PC_Infos
-Tool to get System infos. First version will be with CMD Interface, a GUI Will be created later. 
+Tool to get System infos for mac OS. First version will be with CMD Interface, a GUI Will be created later. 
