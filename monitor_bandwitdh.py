0ff486031141c41f1e2bac097b553ce86c6f3e29
Alexs <alexiscaronris@outlook.fr>
2021-01-21 13:24:21 +0100

    Séparation des volumes en Download et Upload et clear pour que ce soit plus propre

--- monitor_bandwitdh.py
+++ monitor_bandwitdh.py
@@ -1,23 +1,29 @@
 import time
-import psutil
+import psutil, os
 
 def main():
-    old_value = 0
-
+    old_Upload_volume = 0
+    old_Download_volume = 0
     while True:
-        new_value = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
+        Upload_volume = psutil.net_io_counters().bytes_sent
+        Download_volume = psutil.net_io_counters().bytes_recv
 
-        if old_value:
-            send_stat(new_value - old_value)
+        if Download_volume:
+            send_stat_Upload(Upload_volume - old_Upload_volume)
+            send_stat_Download(Download_volume - old_Download_volume)
 
-        old_value = new_value
+        old_Upload_volume = Upload_volume
+        old_Download_volume = Download_volume
 
         time.sleep(1)
-
+        os.system('clear')
 def convert_to_Mo(value):
     return value/1024./1024.
 
-def send_stat(value):
-    print ("%0.3f" % convert_to_Mo(value) + " Mo")
+def send_stat_Download(value):
+    print ("Download : " + "%0.3f" % convert_to_Mo(value) + " Mo")
+
+def send_stat_Upload(value):
+    print ("Upload :" + "%0.3f" % convert_to_Mo(value) + " Mo")
 
 main()
